d82068c47e4b29207a24cd01db5f682c79f52a24
CloudyDirk <Guenther_D@de.cenit-group.com>
2016-08-07 23:41:09 +0200

    Initialize only once

--- src/main/java/de/drtodolittle/firebase/impl/Activator.java
+++ src/main/java/de/drtodolittle/firebase/impl/Activator.java
@@ -22,28 +22,33 @@ import de.drtodolittle.firebase.api.TokenService;
  */
 public class Activator implements BundleActivator {
 
-
 	private ServiceRegistration<TokenService> service;
 
-	/* (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.
+	 * BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
-		FirebaseOptions options = new FirebaseOptions.Builder()
-				  .setServiceAccount(new FileInputStream("DrToDoLittle-10b9f17b6eb3.json"))
-				  .setDatabaseUrl("https://drtodolittle.firebaseio.com/")
-				  .build();
-		FirebaseApp.initializeApp(options);
+		if (FirebaseApp.getApps().isEmpty()) {
+			FirebaseOptions options = new FirebaseOptions.Builder()
+					.setServiceAccount(new FileInputStream("DrToDoLittle-10b9f17b6eb3.json"))
+					.setDatabaseUrl("https://drtodolittle.firebaseio.com/").build();
+			FirebaseApp.initializeApp(options);
+		}
 		Hashtable<String, Object> properties = new Hashtable<String, Object>();
 		properties.put(Constants.SERVICE_PID, FirebaseTokenService.PID);
 		service = context.registerService(TokenService.class, new FirebaseTokenService(), properties);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
-		FirebaseApp.getApps().clear();
 		service.unregister();
 	}
 
